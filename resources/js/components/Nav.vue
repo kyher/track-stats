600fd331b6c6fa719a89cc75cbb202634f34dfd5
kyher <kyle.heron@pm.me>
2025-11-17 16:36:14 +0000

    Layout changes

---
+++ resources/js/components/Nav.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { dashboard, login, logout, register } from '@/routes';
+import { Link } from '@inertiajs/vue3';
+</script>
+<template>
+    <nav class="flex w-full items-center gap-4">
+        <Link
+            href="/"
+            class="inline-block rounded-sm px-5 py-1.5 text-lg leading-normal font-bold text-[#1b1b18] hover:scale-105 dark:text-[#EDEDEC]"
+        >
+            TrackStats</Link
+        >
+        <div class="ml-auto flex items-center gap-4">
+            <Link
+                v-if="$page.props.auth.user"
+                :href="dashboard()"
+                class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+            >
+                Dashboard
+            </Link>
+            <Link
+                v-if="$page.props.auth.user"
+                :href="logout()"
+                class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                >Log out
+            </Link>
+            <template v-else>
+                <Link
+                    :href="login()"
+                    class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                >
+                    Log in
+                </Link>
+                <Link
+                    :href="register()"
+                    class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                >
+                    Register
+                </Link>
+            </template>
+        </div>
+    </nav>
+</template>
--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
-import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
-import type { BreadcrumbItemType } from '@/types';
-
-interface Props {
-    breadcrumbs?: BreadcrumbItemType[];
-}
-
-withDefaults(defineProps<Props>(), {
-    breadcrumbs: () => [],
-});
+import Nav from '@/components/Nav.vue';
 </script>
-
 <template>
-    <AppLayout :breadcrumbs="breadcrumbs">
-        <slot />
-    </AppLayout>
+    <div
+        class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]"
+    >
+        <Nav />
+        <main class="text-white">
+            <h1 class="text-3xl font-bold">TrackStats</h1>
+            <slot />
+        </main>
+    </div>
 </template>
