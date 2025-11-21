688a7f45f17fe9059fad282672fda2d131e5b623
kyher <kyle.heron@pm.me>
2025-11-21 17:34:30 +0000

    Update TrackSeeder

--- database/seeders/TrackSeeder.php
+++ database/seeders/TrackSeeder.php
@@ -12,6 +12,35 @@ class TrackSeeder extends Seeder
      */
     public function run(): void
     {
-        Track::factory()->count(50)->create();
+        $tracks = [
+            "ALBERT PARK",
+            "SHANGHAI INTERNATIONAL CIRCUIT",
+            "SUZUKA CIRCUIT",
+            "BAHRAIN INTERNATIONAL CIRCUIT",
+            "JEDDAH STREET CIRCUIT",
+            "MIAMI INTERNATIONAL AUTODROME",
+            "AUTODROMO ENZO E DINO FERRARI",
+            "CIRCUIT DE MONACO",
+            "CIRCUIT DE CATALUNYA",
+            "CIRCUIT GILLES VILLENEUVE",
+            "RED BULL RING",
+            "CIRCUIT SILVERSTONE",
+            "SPA-FRANCORCHAMPS",
+            "HUNGARORING",
+            "CIRCUIT ZANDVOORT",
+            "AUTODROMO NAZIONALE MONZA",
+            "BAKU CITY CIRCUIT",
+            "MARINA BAY STREET CIRCUIT",
+            "CIRCUIT OF THE AMERICAS",
+            "AUTODROMO HERMANOS RODRIGUEZ",
+            "AUTODROMO JOSE CARLOS PACE INTERLAGOS",
+            "LAS VEGAS STREET CIRCUIT",
+            "LOSAIL INTERNATIONAL CIRCUIT",
+            "YAS MARINA CIRCUIT"
+        ];
+
+        foreach ($tracks as $trackName) {
+            Track::create(['name' => $trackName]);
+        }
     }
 }
