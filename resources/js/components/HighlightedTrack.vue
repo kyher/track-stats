6ed11b293b10ff55cc6c292b94201b14e1381c9d
kyher <kyle.heron@pm.me>
2025-11-24 16:43:37 +0000

    Improve HighlightedTrack styling

--- resources/js/components/HighlightedTrack.vue
+++ resources/js/components/HighlightedTrack.vue
@@ -9,9 +9,14 @@ const { track } = props;
 </script>
 <template>
     <div
-        class="my-8 mb-4 rounded border-2 border-yellow-500 p-4 font-bold shadow-lg transition-shadow duration-300 hover:shadow-2xl"
+        class="my-8 mb-4 rounded border-2 border-yellow-500 bg-yellow-500/10 p-4 font-bold shadow-lg transition-shadow duration-300 hover:shadow-2xl"
     >
-        <p>{{ track.name }}</p>
-        <p class="font-bold">{{ track.voteCount }} votes</p>
+        <div class="flex items-center justify-between">
+            <div>
+                <p>{{ track.name }}</p>
+                <p class="font-bold">{{ track.voteCount }} votes</p>
+            </div>
+            <span class="text-3xl">👑</span>
+        </div>
     </div>
 </template>
