f453ee19ae17d17ad692d10bd4437769a1b0fa5f
kyher <kyle.heron@pm.me>
2025-11-27 16:22:28 +0000

    Add TrackController

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,12 @@
 <?php
 
+use App\Http\Controllers\TrackController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 use App\Models\Track;
 use App\Http\Controllers\VoteController;
 
-Route::get('/', function () {
-    $tracks = Track::all();
-    $highestVotedTrack = $tracks->sortByDesc(fn($track) => $track->votes->count())->first();
-    if ($highestVotedTrack instanceof Track) {
-        $restOfTracks = $tracks->where('id', '!=', $highestVotedTrack?->id)
-            ->sortByDesc(fn($track) => $track->votes->count())->values();
-
-        $otherTracksWithTheSameVoteCount = $restOfTracks->filter(fn($track) => $track->votes->count() === $highestVotedTrack?->votes->count());
-
-        if ($otherTracksWithTheSameVoteCount->isNotEmpty()) {
-            $highestVotedTracks = collect([$highestVotedTrack])->merge($otherTracksWithTheSameVoteCount);
-            // Remove these from the rest of the tracks
-            $restOfTracks = $restOfTracks->whereNotIn('id', $highestVotedTracks->pluck('id'))->values();
-        } else {
-            $highestVotedTracks = collect([$highestVotedTrack]);
-        }
-    }
-
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-        'highestVotedTracks' => $highestVotedTracks ?? [],
-        'tracks' => $restOfTracks ?? [],
-    ]);
-})->name('home');
+Route::get('/', [TrackController::class, 'index'])->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard', [
